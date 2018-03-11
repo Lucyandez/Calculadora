64a2696ccfc7d909c9cc3fa0f1b349615e38541e
Olga Hernandez <lucyandez@gmail.com>
2018-03-11 18:42:17 -0500

    Se agregaron eventos a los botones y se crearon funciones para realizar las operaciones

--- app/src/main/java/com/example/diseno/calculadora/MainActivity.java
+++ app/src/main/java/com/example/diseno/calculadora/MainActivity.java
@@ -14,67 +14,143 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        info= findViewById(R.id.resultado);
+        info = findViewById(R.id.resultado);
     }
 
-    public void btn0 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"0";
+    public void btnpunto (View view){
+        String captura = info.getText().toString();
+        captura = captura+".";
         info.setText(captura);
     }
 
-    public void btn1 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"1";
+    public void btn0 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"0";
         info.setText(captura);
     }
 
-    public void btn2 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"2";
+    public void btn1 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"1";
         info.setText(captura);
     }
 
-    public void btn3 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"3";
+    public void btn2 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"2";
         info.setText(captura);
     }
 
-    public void btn4 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"4";
+    public void btn3 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"3";
         info.setText(captura);
     }
 
-    public void btn5 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"5";
+    public void btn4 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"4";
         info.setText(captura);
     }
 
-    public void btn6 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"6";
+    public void btn5 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"5";
         info.setText(captura);
     }
 
-    public void btn7 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"7";
+    public void btn6 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"6";
         info.setText(captura);
     }
 
-    public void btn8 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"8";
+    public void btn7 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"7";
         info.setText(captura);
     }
 
-    public void btn9 (View v){
-        String captura= info.getText().toString();
-        captura=captura+"9";
+    public void btn8 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"8";
         info.setText(captura);
     }
 
+    public void btn9 (View view){
+        String captura = info.getText().toString();
+        captura = captura+"9";
+        info.setText(captura);
+    }
+
+    public void primerNum(int view){
+        try{
+            String contadorA = info.getText().toString();
+            primero = Double.parseDouble(contadorA);
+        }catch(NumberFormatException nfe){};
+        info.setText("");
+    }
+
+    public void suma(View view){
+        primerNum(0);
+        operacion=0;
+    }
+
+    public void resta(View view){
+        primerNum(0);
+        operacion=1;
+    }
+
+    public void multiplica(View view){
+        primerNum(0);
+        operacion=2;
+    }
+
+    public void dividir(View view){
+        primerNum(0);
+        operacion=3;
+    }
+
+    public void igual (View view){
+        try{
+            String contadorB = info.getText().toString();
+            segundo = Double.parseDouble(contadorB);
+        }catch(NumberFormatException nfe){};
+        info.setText("");
+
+        switch (operacion){
+            case 0:{
+                inforesult = primero+segundo;
+                info.setText(""+inforesult);
+                break;
+            }
+            case 1:{
+                inforesult = primero-segundo;
+                info.setText(""+inforesult);
+                break;
+            }
+            case 2:{
+                inforesult = primero*segundo;
+                info.setText(""+inforesult);
+                break;
+            }
+            case 3:{
+                if(segundo==0){
+                    info.setText("Error");
+                }else{
+                    inforesult = primero/segundo;
+                    info.setText(""+inforesult);
+                }
+                break;
+            }
+        }
+        primero=inforesult;
+    }
+
+    public void ac (View v){
+        info.setText("");
+        primero=0.0;
+        segundo=0.0;
+        inforesult=0.0;
+    }
 }
